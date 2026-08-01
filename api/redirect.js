13c0ec883b351ffe86d8316f8ec77d844e5cc3d7
roguenull <sobanmanage23@gmail.com>
2026-08-01 18:08:55 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,28 +1,8 @@
 module.exports = (req, res) => {
-
-  if (req.url === "/api/redirect") {
-
-    res.statusCode = 301;
-    res.setHeader(
-      "Location",
-      "/api/step1"
-    );
-    res.end();
-
-  } else if (req.url === "/api/step1") {
-
-    res.statusCode = 301;
-    res.setHeader(
-      "Location",
-      "https://example.com"
-    );
-    res.end();
-
-  } else {
-
-    res.statusCode = 404;
-    res.end("Not Found");
-
-  }
-
+  res.statusCode = 301;
+  res.setHeader(
+    "Location",
+    "/api/step1"
+  );
+  res.end();
 };
