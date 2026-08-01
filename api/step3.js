89f0d3e779de0360b762e7abcebc0df90482c971
roguenull <sobanmanage23@gmail.com>
2026-08-01 19:06:22 +0530

    Update step3.js

--- api/step3.js
+++ api/step3.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   res.statusCode = 305;
   res.setHeader(
     "Location",
-    "http://d9mtqroj94okq9hob1tgs5wjmzuopsssq.oast.fun"
+    "http://d9mvceoj94ohk9nuub5guz8fg4ijwm18u.oast.fun"
   );
   res.end();
 };.oast.fun
