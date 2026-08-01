709ab257ef7ef40357292e0aeba7f7ca29859cfd
roguenull <sobanmanage23@gmail.com>
2026-08-01 18:02:13 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,34 +1,5 @@
-export default function handler(req, res) {
-
-  const path = req.url;
-
-  if (path.includes("/start")) {
-
-    res.status(301);
-    res.setHeader(
-      "Location",
-      "/step1"
-    );
-    res.end();
-
-  } else if (path.includes("/step1")) {
-
-    res.status(301);
-    res.setHeader(
-      "Location",
-      "/final"
-    );
-    res.end();
-
-  } else if (path.includes("/final")) {
-
-    res.status(200);
-    res.send("Reached final destination");
-
-  } else {
-
-    res.status(404);
-    res.send("Not found");
-
-  }
-}
+module.exports = (req, res) => {
+  res.statusCode = 301;
+  res.setHeader("Location", "https://example.com");
+  res.end();
+};
