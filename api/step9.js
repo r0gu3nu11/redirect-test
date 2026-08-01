4b60193da60c8e065a3ef19c813af90fadd8f54c
roguenull <sobanmanage23@gmail.com>
2026-08-01 19:43:03 +0530

    Update step9.js

--- api/step9.js
+++ api/step9.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   res.statusCode = 308;
   res.setHeader(
     "Location",
-    "http://make-1.2.3.4-rebind-169.254-169.254-rr.1u.ms"
+    "http://løcalhäst"
   );
   res.end();
 };
