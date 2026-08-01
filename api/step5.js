9417ff3cae97d0fa8d17c3fe340fcafa3d92342a
roguenull <sobanmanage23@gmail.com>
2026-08-01 18:57:53 +0530

    Update step5.js

--- api/step5.js
+++ api/step5.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  res.statusCode = 307;
+  res.statusCode = 308;
   res.setHeader(
     "Location",
     "https://d9mtqroj94okq9hob1tgs5wjmzuopsssq.oast.fun/robots.txt?test=test.png"
