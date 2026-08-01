a117d404d5e924bb42b45ad9f97761753a7fa1ba
roguenull <sobanmanage23@gmail.com>
2026-08-01 21:32:38 +0530

    Update step3.js

--- api/step3.js
+++ api/step3.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   res.statusCode = 301;
   res.setHeader(
     "Location",
-    "http://make-127.0.0.1-rebind-127.0-0.1-rr.1u.ms:008080/config.php?test=testxsdffs.png"
+    "http://[::]:008080/config.php?test=testxsdffs.png"
   );
   res.end();
 };
