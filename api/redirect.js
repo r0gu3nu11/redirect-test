191b7f38e3c028c7153736c284ad8e852f2c01db
roguenull <sobanmanage23@gmail.com>
2026-08-01 17:59:48 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
 
   const path = req.url;
 
-  if (path.includes("/step1")) {
+  if (path.includes("/start")) {
 
     res.status(301);
     res.setHeader(
@@ -11,53 +11,21 @@ export default function handler(req, res) {
     );
     res.end();
 
-  } else if (path.includes("/step2")) {
+  } else if (path.includes("/step1")) {
 
-    res.status(302);
+    res.status(301);
     res.setHeader(
       "Location",
-      "/step3"
+      "/final"
     );
     res.end();
 
-  } else if (path.includes("/step3")) {
+  } else if (path.includes("/final")) {
 
-     res.status(303);
-    res.setHeader(
-      "Location",
-      "/step4"
-    );
-    res.end();
+    res.status(200);
+    res.send("Reached final destination");
 
-  }  else if (path.includes("/step4")) {
-
-    res.status(304);
-    res.setHeader(
-      "Location",
-      "/step5"
-    );
-    res.end();
-
-  }  else if (path.includes("/step5")) {
-
-    res.status(304);
-    res.setHeader(
-      "Location",
-      "/step6"
-    );
-    res.end();
-
-  } else if (path.includes("/step6")) {
-
-    res.status(304);
-    res.setHeader(
-      "Location",
-      "http://localhost:8080/"
-    );
-    res.end();
-
-  }
-  else {
+  } else {
 
     res.status(404);
     res.send("Not found");
